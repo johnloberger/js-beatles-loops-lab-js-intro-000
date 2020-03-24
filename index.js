aca4a9db10e63affd328ced55df2da8b606c8109
John Loberger <johnloberger@gmail.com>
2020-03-24 20:15:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function johnLennonFacts(array) {
   }
   return array;
 }
+function iLoveTheBeatles(number) {
+  var arr = [];
+  var i = 0;
+  do {
+    arr.push('I love the Beatles!');
+    i++;
+  } while (i <= number && number < 15);
+  return arr;
+}
