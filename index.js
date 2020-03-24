6f270f755041605e8b1eba8b1f5f78974847f0a9
John Loberger <johnloberger@gmail.com>
2020-03-24 20:11:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ function theBeatlesPlay(musicians, instruments){
   }
   return array
 }
-function johnLennonFacts(){
-  var facts = ["He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
-  var array = []
-  var i = 0
-  while (array.length < facts.length){
-    array.push(facts[i] + "!!!")
-  }
-  return array
+const facts=[
+“He was the last Beatle to learn to drive”,
+“He was never a vegetarian”,
+“He was a choir boy and boy scout”,
+“He hated the sound of his own voice”
+];
+function johnLennonFacts(factsArray){
+var i=0;
+var newArray=[];
+while (factsArray.length > i){
+newArray.push(`${factsArray[i]}` + “!!!”);
+i++;
+}
+return newArray;
 }
