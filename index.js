81627e073882260b0a2fcb75e5b009b2d361c932
John Loberger <johnloberger@gmail.com>
2020-03-24 20:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function theBeatlesPlay(musicians, instruments){
   }
   return array
 }
-const facts=[
-“He was the last Beatle to learn to drive”,
+const facts = [“He was the last Beatle to learn to drive”,
 “He was never a vegetarian”,
 “He was a choir boy and boy scout”,
 “He hated the sound of his own voice”
